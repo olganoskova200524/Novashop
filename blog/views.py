91508b168e9e1e9917948a9b3c97d2d0ecfb0154
Olga Noskova <olga.tolba@gmail.com>
2025-11-06 19:03:57 +1000

    feat(blog): full CRUD with CBV and view counter

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.db import models
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True).order_by('-created_at')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        Post.objects.filter(pk=obj.pk).update(views=models.F('views') + 1)  # инкремент просмотров
+        obj.refresh_from_db(fields=['views'])
+        return obj
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/post_form.html'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/post_form.html'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:list')
