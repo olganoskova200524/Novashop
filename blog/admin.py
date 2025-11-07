fd8ab47b3a2a9cf1d809784777f747c1b7dfca26
Olga Noskova <olga.tolba@gmail.com>
2025-11-07 10:01:53 +1000

    Admin: register Post with list_display, filters, search

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'created_at', 'views')
+    list_filter = ('is_published',)
+    search_fields = ('title', 'content')
