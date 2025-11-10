d4f077860c5816d4ab6f605c8bb9bdc33bb236aa
Olga Noskova <olga.tolba@gmail.com>
2025-11-10 11:36:39 +1000

    style(forms): add bootstrap styling via __init__ for ProductForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -24,6 +24,42 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'description', 'price', 'category', 'image']
 
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        # Поле "Название"
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название товара'
+        })
+
+        # Настройка атрибутов виджета для поля "Описание"
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите описание товара (без запрещённых слов)',
+            'rows': 4
+        })
+
+        # Настройка атрибутов виджета для поля "Цена"
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите цену товара',
+            'min': '0',
+            'step': '0.01',
+            'type': 'number'
+        })
+
+        # Настройка атрибутов виджета для поля "Категория"
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-select'
+        })
+
+        # Настройка атрибутов виджета для поля "Изображение"
+        self.fields['image'].widget.attrs.update({
+            'class': 'form-control',
+            'accept': 'image/*'
+        })
+
     def clean_name(self):
         value = self.cleaned_data.get('name')
         _validate_forbidden(value, 'Название')
