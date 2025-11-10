057a1dc7c113c5f0781cc6a793e7b66b7c58e5dc
Olga Noskova <olga.tolba@gmail.com>
2025-11-10 11:06:55 +1000

    feat(catalog): routes and templates for product CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ContactsView
+from .views import ProductListView, ProductDetailView, ContactsView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = 'catalog'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/create/', ProductCreateView.as_view(), name='create'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='edit'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='delete'),
 ]
