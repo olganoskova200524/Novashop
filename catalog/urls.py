86adc6154cee9aac56b30ed9fc5083bf3d6ba9b8
Olga Noskova <olga.tolba@gmail.com>
2025-11-23 19:00:42 +1000

    feat(caching): add product category service, view and caching for product list

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 from .views import ProductListView, ProductDetailView, ContactsView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView, ProductUnpublishView
+    ProductDeleteView, ProductUnpublishView, CategoryProductsView
+
+from django.conf import settings
+from django.views.decorators.cache import cache_page
 
 app_name = 'catalog'
 
+product_detail_view = ProductDetailView.as_view()
+
+if settings.CACHE_ENABLED:
+    product_detail_view = cache_page(60 * 5)(product_detail_view)
+
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
+    path('product/<int:pk>/', product_detail_view, name='detail'),
+    path('category/<int:category_id>/', CategoryProductsView.as_view(), name='category_products'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/create/', ProductCreateView.as_view(), name='create'),
     path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='edit'),
--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,8 @@ from django.views import View
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from .forms import ProductForm
-from .models import Product
+from .models import Product, Category
+from .services import get_products_by_category
 
 
 class ProductUnpublishView(PermissionRequiredMixin, View):
@@ -74,3 +75,19 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
 
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
+
+
+class CategoryProductsView(ListView):
+    model = Product
+    template_name = 'catalog/category_products.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return get_products_by_category(category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs['category_id']
+        context['category'] = Category.objects.get(pk=category_id)
+        return context
