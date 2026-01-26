bb785f2226a4255de6517ac9e5384dc5f73b6fa9
Olga Noskova <olga.tolba@gmail.com>
2026-01-26 19:22:54 +1000

    test: add basic blog test and update CI

--- blog/tests.py
+++ blog/tests.py
@@ -1,2 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BlogSmokeTest(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
